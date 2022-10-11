86084843a361052a48cf5ab0307183c0e0191256
Adam Evans <primeae@amazon.com>
2022-10-11 16:52:09 +0100

    Adding MemoryGame.swift to codebase with basic structs inside

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Evans, Adam on 11/10/2022.
+//
+
+import Foundation
+
+struct MemoryGame{
+    var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        
+    }
+}
