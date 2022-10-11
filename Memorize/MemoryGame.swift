b20648c623d2e33fee47c69458cc4c7c712f8695
Adam Evans <primeae@amazon.com>
2022-10-11 17:09:05 +0100

    Adding an initialiser and setting default values for card struct

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -8,15 +8,25 @@
 import Foundation
 
 struct MemoryGame<CardContent>{
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
     func choose(_ card: Card) {
         
     }
     
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent){
+        cards = Array<Card>()
+        // add numberOfPairsOfCards x2 to cards array
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content: CardContent = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card {
-        var isFaceUp: Bool
-        var isMathced: Bool
+        var isFaceUp: Bool = false
+        var isMathced: Bool = false
         var content: CardContent
     }
 }
