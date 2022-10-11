705b4fe4573b443e17455b3fd515c7f3a12b98c7
Adam Evans <primeae@amazon.com>
2022-10-11 17:21:37 +0100

    Using type functions and type variables to initialise EmojiMemoryGame

--- EmojiMemoryGame.swift
+++ EmojiMemoryGame.swift
@@ -5,4 +5,21 @@
 //  Created by Evans, Adam on 11/10/2022.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    
+    static let emojis = ["🚌", "🛻", "🚝", "🚔", "🛴", "🚲", "🏍", "🛺", "🚕", "✈️", "🚀", "🛸", "🚁", "🛶", "⛴", "⛵️", "🛰", "🚂", "🚋", "🦼"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
