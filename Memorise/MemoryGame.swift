be65425d2c61d8cd0a356e8d0c7b90782f0f25d4
Adam Evans <primeae@amazon.com>
2022-10-12 15:35:42 +0100

    Using firstIndex inside of custom made func

--- Memorise/MemoryGame.swift
+++ Memorise/MemoryGame.swift
@@ -11,17 +11,10 @@ struct MemoryGame<CardContent>{
     private(set) var cards: Array<Card>
     
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
-    }
-    
-    func index(of card: Card) -> Int {
-        for index in 0..<cards.count {
-            if cards[index].id == card.id {
-                return index
-            }
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) {
+            cards[chosenIndex].isFaceUp.toggle()
         }
-        return 0 // Fix
+        print("\(cards)")   // same as %s in python
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent){
